67f8756dc0a9c23843aa529ec07b856c84638c60
09FirdaMardiaturrahman <101926397+09FirdaMardiaturrahman@users.noreply.github.com>
2022-04-09 01:35:12 +0700

    Add files via upload

---
+++ firda proses.cpp
@@ -0,0 +1,31 @@
+using namespace std;
+
+class proses {
+  public :
+    void cetak(){
+      cout<< "Masukkan jumlah pesanan Shared Hosting :";
+      cin>>shrd_hstng;
+      cout<<" Masukkan jumlah pesanan VPS Server :";
+      cin>>vps_srvr;
+      cout<< " Masukkan jumlah pesanan Cloud Server :";
+      cin>>cld_srvr;
+      cout<<" Masukkan jumlah pesanan Domain name :";
+      cin>>dmn_nm;
+
+    total1 = 300000 * Shared Hosting;
+    total2 = 930000 * VPS Server;
+    total3 = 500000 * Cloud Server;
+    total4 = 280000 * Domain name;
+    diskon = (total * 10 / 100);
+    totals  = total - diskon; 
+      }
+  else if (total >= 10000000){
+  diskon = (total * 10 / 100);
+  totals = total - diskon;
+  }
+private:
+//ofstream tulis_data;
+
+int 
+Shared Hosting,VPS Server,Cloud Server,Domain name,total1,total2,total3,total4,jarak,diskon,total,bayar,bayar,kembali;
+}; 
