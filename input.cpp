25b91c709f9a59c7e31770093a0204b22a6906cc
adeliasyarla <101863106+adeliasyarla@users.noreply.github.com>
2022-04-09 01:32:29 +0700

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+
+class Input {
+  public:
+    void cetak (){
+      cout<<" 1) Shared Hosting : Rp.300000"<<endl;
+      cout<<" 2) VPS Server : Rp.930000"<<endl;
+      cout<<" 3) Cloud Server : Rp.5000000"<<endl;
+      cout<<" 4) Domain name :Rp.280000"<<endl;
+    }    
+private:
+//ofstream tulis_data;
+
+int 
+Shared Hosting,VPS Server,Cloud Server,Domain name,total1,total2,total3,total4,jarak,diskon,total,bayar,bayar,kembali;
+};
+
